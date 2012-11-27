2b9877637fbe12c26174b7b9f08ff6b7b7255cdc
Brandon Hilkert <brandonhilkert@gmail.com>
2012-11-27 11:17:26 -0500

    Remove unnecessary files/folders.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Grocery::Application.routes.draw do
-  get "lists/index"
-
   root to: "lists#index"
-
   resources :lists, only: [:index, :create] do
     resources :items, only: [:index, :create, :destroy]
   end
